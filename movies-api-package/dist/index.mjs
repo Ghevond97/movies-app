cad7a11ea2a985fed37f5cc86a91fdc2d6816855
Ghevond97 <ghevond.mirzakhanyan@gmail.com>
2024-04-09 01:21:59 +0400

    trending movies api call moved to npm pacakage, tested for working, corresponding type definitions, and type interfaces created for the api call, test api call implemented inside app tsx

--- movies-api-package/dist/index.mjs
+++ movies-api-package/dist/index.mjs
@@ -1,9 +1,44 @@
+var __async = (__this, __arguments, generator) => {
+  return new Promise((resolve, reject) => {
+    var fulfilled = (value) => {
+      try {
+        step(generator.next(value));
+      } catch (e) {
+        reject(e);
+      }
+    };
+    var rejected = (value) => {
+      try {
+        step(generator.throw(value));
+      } catch (e) {
+        reject(e);
+      }
+    };
+    var step = (x) => x.done ? resolve(x.value) : Promise.resolve(x.value).then(fulfilled, rejected);
+    step((generator = generator.apply(__this, __arguments)).next());
+  });
+};
+
 // src/index.ts
 function add(a, b) {
   return a + b;
 }
 console.log(add(3, 5));
+function fetchData(apiKey, signal) {
+  return __async(this, null, function* () {
+    console.log("FETCHING");
+    const response = yield fetch(
+      `
+  https://api.themoviedb.org/3/trending/all/day?api_key=${apiKey}`,
+      { signal }
+    );
+    console.log("RESP", response);
+    const data = yield response.json();
+    return data;
+  });
+}
 export {
-  add
+  add,
+  fetchData
 };
 //# sourceMappingURL=index.mjs.map
--- movies-api-package/src/index.ts
+++ movies-api-package/src/index.ts
@@ -3,11 +3,41 @@ export function add(a: number, b: number): number {
 }
 
 console.log(add(3, 5)); //output: 8
+export interface TrendingMovieData {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  media_type: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
 
-// export async function fetchData(apiKey: string): Promise<any> {
-//   const response = await fetch(
-//     `https://api.example.com/data?api_key=${apiKey}`
-//   );
-//   const data = await response.json();
-//   return data;
-// }
+interface ApiResponse {
+  page: number;
+  results: TrendingMovieData[];
+  total_pages: number;
+  total_results: number;
+}
+export async function fetchData(
+  apiKey: string,
+  signal: AbortController["signal"]
+): Promise<ApiResponse> {
+  console.log("FETCHING");
+  const response = await fetch(
+    `
+  https://api.themoviedb.org/3/trending/all/day?api_key=${apiKey}`,
+    { signal }
+  );
+  console.log("RESP", response);
+  const data = await response.json();
+  return data;
+}
